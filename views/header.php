05f5ea89978460c3e8dc74c8129434e84f4e48c9
Ynryo <ewenatideklein@gmail.com>
2025-12-12 10:52:25 +0100

    commit

--- views/header.php
+++ views/header.php
@@ -5,26 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>La Crêpe Dorée - Crêperie Artisanale</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="assets/css/globals.css">
     <link rel="stylesheet" href="assets/css/header.css">
-    <link rel="icon" type="image/x-icon" href="https://placehold.co/16x16/red/white?text=CD">
 </head>
 
 <body>
     <header>
         <div>
-            <div>
-                <div>
-                    <a href="/">La Crêpe Dorée</a>
-                </div>
-                <nav class="hidden md:flex space-x-8">
-                    <a href="/carte">Menu</a>
-                    <a href="/about">À Propos</a>
-                    <a href="#reservation">Réserver</a>
-                    <a href="#contact">Contact</a>
-                </nav>
-            </div>
+            <a href="/">La Crêpe Dorée</a>
         </div>
+        <nav>
+            <a href="/carte">Menu</a>
+            <a href="/about">À Propos</a>
+            <a href="/reservation">Réserver</a>
+            <a href="/contact">Contact</a>
+        </nav>
     </header>
 </body>
 
