02cef2125687af39a14e640ace22faf6d9471d50
Matthieu Marhadour <i2401827@cri.univ-lemans.fr>
2025-12-12 09:41:32 +0100

    ajout du formulaire basique

---
+++ views/contact.php
@@ -0,0 +1,25 @@
+
+<body>
+
+    <h1>Contactez-nous</h1>
+
+    <form action="traitement.php" method="post">
+        
+        <label for="nom">Nom :</label><br>
+        <input type="text" id="nom" name="nom" required><br><br>
+
+        <label for="email">Email :</label><br>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <label for="sujet">Sujet :</label><br>
+        <input type="text" id="sujet" name="sujet" required><br><br>
+
+        <label for="message">Message :</label><br>
+        <textarea id="message" name="message" rows="5" required></textarea><br><br>
+
+        <button type="submit">Envoyer</button>
+
+    </form>
+
+</body>
+
