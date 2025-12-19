96ec1df061f4bea8330e48589422bef65a09efeb
Saucichef <arthur.paillereau@gmail.com>
2025-12-19 09:16:39 +0100

    push index

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 
 $page = $_GET['page'] ?? 'acceuil';
 // faire le switch après
