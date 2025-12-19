4f93c7a39553d1970f0f3ff2b340385384f38750
Metox <matthieu.marhadour@gmail.com>
2025-12-19 08:24:03 +0100

    routeur fonctionnelle plus correction du modèles MVC

--- index.php
+++ index.php
@@ -2,15 +2,12 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 
-$page = $_GET['page'];
+$page = $_GET['page'] ?? 'acceuil';
 // faire le switch après
 
-$url = $_SERVER['REQUEST_URI'];
-if ($url === '/' || $url === '/index.php') {
-    require_once "controllers/homeController.php";
-} else if ($url === '/carte') {
-    require_once "controllers/carteController.php";
-} else {
-    http_response_code(404);
-    echo "Page non trouvée.";
+switch($page){
+    case 'acceuil':
+        require_once __DIR__.'/views/header.php';
 }
+
+
