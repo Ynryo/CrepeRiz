41403c028fcef4aeed8228095ae9be7929928eb4
Ynryo <ewenatideklein@gmail.com>
2025-12-19 10:08:20 +0100

    added contact an default nav

--- index.php
+++ index.php
@@ -5,16 +5,19 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 
-$page = $_GET['page'] ?? 'acceuil';
-// faire le switch après
+$page = $_GET['page'] ?? 'accueil';
 
+// faire le switch après
 switch($page){
-    case 'acceuil':
+    case 'accueil':
         require_once __DIR__.'/controllers/homeController.php';
+        break;
     case 'carte':
         require_once __DIR__.'/controllers/carteController.php';
-
-
+        break;
+    case 'contact':
+        require_once __DIR__.'/controllers/contactController.php';
+        break;
+    default:
+        require_once __DIR__.'/controllers/homeController.php';
 }
-
-
