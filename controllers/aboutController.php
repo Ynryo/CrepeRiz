e199a27a12f365bed71e088ae872ecb6213b734a
Saucichef <arthur.paillereau@gmail.com>
2026-01-09 09:00:51 +0100

    newsletter

--- controllers/aboutController.php
+++ controllers/aboutController.php
@@ -1,4 +1,4 @@
 <?php
-require_once __DIR__.'/../views/header.php';
-require_once __DIR__.'/../views/presentationView.php';
-require_once __DIR__.'/../views/footer.php';
+require_once __DIR__ . '/../views/header.php';
+require_once __DIR__ . '/../views/presentationView.php';
+require_once __DIR__ . '/../views/footer.php';
